effc5c23c79a43436abbb515627af6f43f1b87d8
Geovana Santos <geovanaj.s.profissional@gmail.com>
2024-10-21 23:20:23 -0300

    feat: adicionar ContagemRegressiva

---
+++ ContagemRegressiva.java
@@ -0,0 +1,13 @@
+package exercicios.lacos.basico;
+
+public class ContagemRegressiva {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i); // Imprime a contagem de 10 a 1
+        }
+    }
+}
+
+/* Explicação: O programa utiliza um laço for 
+ * que inicia em 10 e decremente até 1, imprimindo cada número.
+ */
